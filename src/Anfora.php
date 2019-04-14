9fe106ead97934447c1be07594a896845bffc5ac
wuding <netjoin@gmail.com>
2019-04-14 16:42:10 +0800

    新增 Anfora 类与函数

---
+++ src/Anfora.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 类文件加载类函数
+ */
+
+class Anfora
+{
+    /**
+     * 构建函数
+     */
+    public function __construct()
+    {
+
+    }
+}
+
+function anfora($file) {
+    return include $file;
+}
